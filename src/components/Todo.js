90b6676eb7b947555d2afeb46e3d588c39020ed7
its-kayes <kayes.ek8@gmail.com>
2022-05-19 01:19:04 +0600

    Load data for diffrent user

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../firebase.init';
 
 const Todo = () => {
+    let [user] = useAuthState(auth);
+    let [tasks, setTasks] = useState();
+    let email = user?.email;
+
+    useEffect(() => {
+
+        fetch(`http://localhost:5000/tasks/${email}`)
+            .then(req => req.json())
+            .then(data => {
+                console.log(data);
+                setTasks(data);
+            })
+
+    }, []);
+
+
     return (
         <div>
-            this is todo part
+            <h1> total task:- {tasks?.length} </h1>
+            <div>
+                <div class="card w-96 bg-base-100 shadow-xl">
+                    <div class="card-body">
+                        <h2 class="card-title">Card title!</h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <div class="card-actions justify-end">
+                            <button class="btn btn-primary">Buy Now</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
